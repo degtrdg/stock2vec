4e10d8feeb7364dc8c65b2e7d840df0b5b4126a3
Daniel George <degtrdg@github.com>
2023-06-07 15:43:45 -0500

    Finish transformer from 8 to 8 We have tensorboard, and our current implementation sucks. We're going to go from 8 to 1 instead.

--- transformers/transformers-for-stocks-torch.py
+++ transformers/transformers-for-stocks-torch.py
@@ -144,56 +144,6 @@ test_set_return = stock[test_start_date:].iloc[:, 3:4].pct_change().values
 print(training_set.shape)
 print(test_set.shape)
 
-# Scaling the training set - I've tried it without scaling and results are very poor.
-sc = MinMaxScaler(feature_range=(0, 1))
-training_set_scaled = sc.fit_transform(training_set)
-
-timesteps = 8
-# First, we create data sets where each sample has with 8 timesteps and 1 output
-# So for each element of training set, we have 8 previous training set elements
-x_train = []
-y_train = []
-
-# subtract 1 because y_train will need one more value
-for i in range(timesteps, training_set.shape[0] - 1):
-    x_train.append(training_set_scaled[i-timesteps:i, 0])
-    # start from i-timesteps+1 to shift one step
-    y_train.append(training_set_scaled[i-timesteps+1:i+1, 0])
-
-# Notice how the first y_train value becomes the last X_train value for the next sample
-x_train, y_train = np.array(x_train), np.array(y_train)
-
-print(x_train[0], y_train[0])
-print(x_train[1], y_train[1])
-
-# Notice how the first y_train value becomes the last X_train value for the next sample
-# Notice how the first y_train value becomes the last X_train value for the next sample
-# print(x_train.shape, y_train.shape)
-x_train = x_train.reshape((x_train.shape[0], x_train.shape[1], 1))
-y_train = y_train.reshape((y_train.shape[0], y_train.shape[1], 1))
-# x_train = x_train.reshape((x_train.shape[0], 1, x_train.shape[1]))
-print(x_train.shape, y_train.shape)
-
-print(x_train.shape, y_train.shape, type(x_train), type(y_train))
-# Interestingly - randomly arranging the samples works well, since we are using validation_split = 0.2, (rather then validation_data = )
-# It is worth looking into whether using a K-fold would work better - if so would not use random permutation.
-idx = np.random.permutation(len(x_train))
-x_train = x_train[idx]
-y_train = y_train[idx]
-
-# Do the same for x_test and y_test
-x_test = []
-y_test = []
-testing_set_scaled = sc.transform(test_set)
-for i in range(timesteps, testing_set_scaled.shape[0] - 1):
-    x_test.append(testing_set_scaled[i-timesteps:i, 0])
-    y_test.append(testing_set_scaled[i-timesteps+1:i+1, 0])
-
-x_test, y_test = np.array(x_test), np.array(y_test)
-x_test = x_test.reshape((x_test.shape[0], x_test.shape[1], 1))
-y_test = y_test.reshape((y_test.shape[0], y_test.shape[1], 1))
-print(x_test.shape, y_test.shape)
-
 
 class TransformerEncoder(nn.Module):
     def __init__(self, input_dim, head_size, num_heads, ff_dim, dropout=0):
@@ -318,6 +268,56 @@ def lr_scheduler(epoch, lr, warmup_epochs=30, decay_epochs=100, initial_lr=1e-6,
     return min_lr
 
 
+# Scaling the training set - I've tried it without scaling and results are very poor.
+sc = MinMaxScaler(feature_range=(0, 1))
+training_set_scaled = sc.fit_transform(training_set)
+
+timesteps = 8
+# First, we create data sets where each sample has with 8 timesteps and 1 output
+# So for each element of training set, we have 8 previous training set elements
+x_train = []
+y_train = []
+
+# subtract 1 because y_train will need one more value
+for i in range(timesteps, training_set.shape[0] - 1):
+    x_train.append(training_set_scaled[i-timesteps:i, 0])
+    # start from i-timesteps+1 to shift one step
+    y_train.append(training_set_scaled[i-timesteps+1:i+1, 0])
+
+# Notice how the first y_train value becomes the last X_train value for the next sample
+x_train, y_train = np.array(x_train), np.array(y_train)
+
+print(x_train[0], y_train[0])
+print(x_train[1], y_train[1])
+
+# Notice how the first y_train value becomes the last X_train value for the next sample
+# Notice how the first y_train value becomes the last X_train value for the next sample
+# print(x_train.shape, y_train.shape)
+x_train = x_train.reshape((x_train.shape[0], x_train.shape[1], 1))
+y_train = y_train.reshape((y_train.shape[0], y_train.shape[1], 1))
+# x_train = x_train.reshape((x_train.shape[0], 1, x_train.shape[1]))
+print(x_train.shape, y_train.shape)
+
+print(x_train.shape, y_train.shape, type(x_train), type(y_train))
+# Interestingly - randomly arranging the samples works well, since we are using validation_split = 0.2, (rather then validation_data = )
+# It is worth looking into whether using a K-fold would work better - if so would not use random permutation.
+idx = np.random.permutation(len(x_train))
+x_train = x_train[idx]
+y_train = y_train[idx]
+
+# Do the same for x_test and y_test
+x_test = []
+y_test = []
+testing_set_scaled = sc.transform(test_set)
+for i in range(timesteps, testing_set_scaled.shape[0] - 1):
+    x_test.append(testing_set_scaled[i-timesteps:i, 0])
+    y_test.append(testing_set_scaled[i-timesteps+1:i+1, 0])
+
+x_test, y_test = np.array(x_test), np.array(y_test)
+x_test = x_test.reshape((x_test.shape[0], x_test.shape[1], 1))
+y_test = y_test.reshape((y_test.shape[0], y_test.shape[1], 1))
+print(x_test.shape, y_test.shape)
+
 # Following is your training part. Make sure your x_train and y_train are torch tensors
 # and are on the right device (cpu or gpu) before passing into the model.
 # assuming x_train is numpy array
@@ -325,6 +325,24 @@ mps_device = torch.device("mps")
 x_train = torch.tensor(x_train, device=mps_device, dtype=torch.float32)
 # assuming y_train is numpy array
 y_train = torch.tensor(y_train, device=mps_device, dtype=torch.float32)
+y_test = torch.tensor(y_test, device=mps_device, dtype=torch.float32)
+x_test = torch.tensor(x_test, device=mps_device, dtype=torch.float32)
+
+# Create a TensorDataset
+train_data = TensorDataset(x_train, y_train)
+
+# Create a TensorDataset for the test data
+print(x_test.shape, y_test.shape)
+test_data = TensorDataset(x_test, y_test)
+
+# Define batch size
+batch_size = 32
+
+# Create a DataLoader
+train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+
+# Create a DataLoader for the test data
+test_loader = DataLoader(test_data, batch_size=batch_size, shuffle=False)
 
 model = Model(
     input_shape=x_train.shape,
@@ -346,25 +364,40 @@ scheduler = LambdaLR(optimizer, lambda epoch: lr_scheduler(
 
 tb = SummaryWriter()
 
+# In your training loop, iterate over train_loader to get batches of data
 epochs = 100  # number of epochs
 for epoch in range(epochs):
-    model.train()  # switch to training mode
-    optimizer.zero_grad()  # reset gradients
+    for i, (x_batch, y_batch) in enumerate(train_loader):
+        model.train()  # switch to training mode
+        optimizer.zero_grad()  # reset gradients
 
-    output = model(x_train)
-    print(f"Output shape: {output.shape}")
-    print(f"y_train shape: {y_train.shape}")
-    loss = criterion(output, y_train)
+        output = model(x_batch)
+        loss = criterion(output, y_batch)
 
-    loss.backward()  # compute gradients
-    optimizer.step()  # update weights
+        loss.backward()  # compute gradients
+        optimizer.step()  # update weights
 
-    scheduler.step()  # update learning rate
+        scheduler.step()  # update learning rate
 
-    tb.add_scalar("Loss", loss, epoch)
+        # Record the loss into the tensorboard
+        tb.add_scalar("Loss", loss.item(), epoch*len(train_loader) + i)
 
-    # optionally print loss here
-    if epoch % 10 == 0:
-        print(f"Epoch: {epoch}, Loss: {loss.item()}")
+        # # optionally print loss here
+        # if i % 10 == 0:
+        #     print(f"Epoch: {epoch}, Batch: {i}, Loss: {loss.item()}")
 
-tb.close()
+    # Evaluation phase
+    # with torch.no_grad():
+    #     total_test_loss = 0
+    #     for i, (x_test_batch, y_test_batch) in enumerate(test_loader):
+    #         model.eval()
+    #         test_output = model(x_test_batch)
+    #         test_loss = criterion(test_output, y_test_batch)
+    #         total_test_loss += test_loss.item()
+
+    #     avg_test_loss = total_test_loss / len(test_loader)
+    #     tb.add_scalar("Test Loss", avg_test_loss, epoch)
+    #     print(f"Epoch: {epoch}, Test Loss: {avg_test_loss}")
+
+
+tb.flush()
